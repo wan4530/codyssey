294b4d5036e8b8d3888c5be77163a3bc87149334
wan4530 <130379941+wan4530@users.noreply.github.com>
2025-03-23 03:40:00 +0900

    Update 1_2.py

--- 1_2/1_2.py
+++ 1_2/1_2.py
@@ -9,7 +9,7 @@ else:
     header = lines[0].strip().split(',')
     log_list = []
     for line in lines[1:]:
-        log_list.append(line.strip().split(",",2))
+        log_list.append(line.strip().split(',',2))
     file.close()
     for i in log_list:
         print(i)
@@ -19,10 +19,10 @@ else:
         log_dict[index] = {header[0]: log[0], header[1]: log[1], header[2]: log[2]}
 
     #json 파일로 저장
-    json_file = open('mission_computer_main.json', 'w',encoding='utf-8')
+    json_file = open('1_2/mission_computer_main.json', 'w',encoding='utf-8')
     json_file.write('{')
     for k,v in log_dict.items():
-        json_file.write(f'\n\t"{k}":{{"{header[0]}": "{v[header[0]]}", "{header[1]}": "{v[header[1]]}", "{header[2]}": "{v[header[2]]}"}},')
+        json_file.write('\n\t"%d":{"%s": "%s", "%s": "%s", "%s": "%s"},'%(k,header[0],v[header[0]],header[1],v[header[1]],header[2],v[header[2]]))
     json_file.seek(json_file.tell()-1)
     json_file.write('\n}')
     json_file.close()   
